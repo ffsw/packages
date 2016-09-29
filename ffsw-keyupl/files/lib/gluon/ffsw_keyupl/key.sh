9a40c37617704914ce063a85077154b94a4c0d2f
Tom <tsiener@gmx.de>
2016-09-30 01:44:46 +0200

    Pfad umbenannt und DOWNLOAD-URL auf ff-sw.net

--- ffsw-keyupl/files/lib/gluon/ffsw_keyupl/key.sh
+++ ffsw-keyupl/files/lib/gluon/ffsw_keyupl/key.sh
@@ -3,7 +3,7 @@
 # this script will upload the nodes fastd key to the gateway server
 # provided as the first argument ($1)
 
-UPLOAD_URL=http://services.freifunk-suedwest.de/keyupload.php
+UPLOAD_URL=http://svc.services.ff-sw.net/keyupload.php
 WGETC=$(which wget)
 
 MAC=$(sed 's/://g' /lib/gluon/core/sysconfig/primary_mac)
--- ffsw-reboot/files/lib/gluon/ffsw-reboot/reboot.sh
+++ ffsw-reboot/files/lib/gluon/ffsw-reboot/reboot.sh
@@ -3,7 +3,7 @@
 # this script will download the reboot conf
 # over mysql database of svc
 
-DOWNLOAD_URL=http://services.freifunk-suedwest.de/reboot.php
+DOWNLOAD_URL=http://svc.services.ff-sw.net/reboot.php
 WGETC=$(which wget)
 changed=0
 
