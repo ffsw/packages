168964d2151cec9a7390ec5bdfc66b060f41209c
Tom <tsiener@gmx.de>
2017-05-20 11:36:43 +0200

    net-rules packag aus v023 übernnommen

--- ffho-ebtables-net-rules/check_site.lua
+++
@@ -1,3 +0,0 @@
-need_string_match('prefix4', '^%d+.%d+.%d+.%d+/%d+$', false)
-need_string_array_match('extra_prefixes6', '^[%x:]+/%d+$', false)
-need_string_array_match('additional_prefix6', '^[%x:]+/%d+$', false)
