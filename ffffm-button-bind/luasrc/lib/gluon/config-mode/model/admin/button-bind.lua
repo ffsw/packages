8667d1a1728d9a96ebf1820625e5153db5f7ea7b
Tom <tsiener@gmx.de>
2018-12-31 00:15:51 +0100

    ffffm-button-bind erneuert
    übernommen aus https://github.com/rubo77/ffm-packages.git

--- ffffm-button-bind/luasrc/lib/gluon/config-mode/model/admin/button-bind.lua
+++ ffffm-button-bind/luasrc/lib/gluon/config-mode/model/admin/button-bind.lua
@@ -1,22 +1,24 @@
 local uci = require("simple-uci").cursor()
 
-local f = Form('Taster')
-local s = f:section(Section, nil, "Hier können dem Router-Taster unterschiedliche Funktionalitäten zugeordnet werden.")
+local f = Form('Wifi-Taster')
+local s = f:section(Section, nil, "Hat der Router einen Wifi-Taster, so kann diesem hier eine Funktion zugeordnet werden:")
 
 local fct = uci:get('button-bind', 'wifi', 'function')
 if not fct then
-	fct='0'
+	fct='1'
 	uci:set('button-bind', 'wifi', 'button')
 	uci:set('button-bind', 'wifi', 'function', fct)
 	uci:commit('button-bind')
 end
 local o = s:option(ListValue, "wifi", "Wifi ON/OFF Taster")
 o.default = fct
-o:value('0', "Wifi an/aus (default)")
-o:value('1', "Funktionslos")
+o:value('0', "Wifi an/aus")
+o:value('1', "Funktionslos (default)")
 o:value('2', "Wifi-Reset")
-o:value('3', "alle Status-LEDs an/aus (Nachtmodus)")
-o:value('4', "Client-Netz an/aus")
+o:value('3', "Nachtmodus 1: alle Status-LEDs an/aus")
+o:value('4', "Nachtmodus 2: LEDs aus, aber während Taster-Betätigung an")
+o:value('5', "Client-Netz an/aus")
+o:value('6', "Mesh-VPN aus für 5 Stunden")
 
 function o:write(data)
 	uci:set('button-bind', 'wifi', 'function', data)
