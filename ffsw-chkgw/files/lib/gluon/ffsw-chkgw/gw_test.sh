d02f93649758a8a0a3206eb410d807f41ca56b99
Tom <tsiener@gmx.de>
2019-06-25 00:03:07 +0200

    gw_test.sh - GW04 added

--- ffsw-chkgw/files/lib/gluon/ffsw-chkgw/gw_test.sh
+++ ffsw-chkgw/files/lib/gluon/ffsw-chkgw/gw_test.sh
@@ -8,15 +8,20 @@ then
 	if [ $? -ne 0 ]
 	then
 		logger -t "ping SVC not ok"
-		ping -c 2 -v6 2a03:2260:300c:300::5> /dev/null 2>&1
+		ping -c 2 -v6 2a03:2260:300c:300::4> /dev/null 2>&1
 		if [ $? -ne 0 ]
 		then
-			logger -t chkgw "ping GW5 not ok"
-			ping -c 2 -v6 2a03:2260:300c:300::6 > /dev/null 2>&1
+			logger -t chkgw "ping GW4 not ok"
+			ping -c 2 -v6 2a03:2260:300c:300::5> /dev/null 2>&1
 			if [ $? -ne 0 ]
 			then
-				logger -t chkgw "ping GW06 not ok - rebooting"
-				reboot
+				logger -t chkgw "ping GW5 not ok"			
+				ping -c 2 -v6 2a03:2260:300c:300::6 > /dev/null 2>&1
+				if [ $? -ne 0 ]
+				then
+					logger -t chkgw "ping GW06 not ok - rebooting"
+					reboot
+				fi
 			fi
 		fi
 	fi
